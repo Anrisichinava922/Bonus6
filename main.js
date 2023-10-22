52cb19ae071e2431a6f5c2f5753a0982f03e6ec8
Anrisichinava922 <anrisichinava922@gmail.com>
2023-10-22 16:03:57 +0400

    Sunnyside

--- main.js
+++
@@ -1,6 +0,0 @@
-const brgr = document.querySelector('.brgr')
-const nonetxt = document.querySelector('.nonetxt')
-
-nonetxt.addEventListener('click', () =>{
-    nonetxt.classlist.toggle(hidden)
-})
